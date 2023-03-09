42a78fea745dbabf504028741b8d8933225ec522
Esilff <vbanari02@gmail.com>
2023-03-09 21:22:55 +0100

    adding glew dependency to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 
 int main() {
     GLFWwindow * window;
     if (!glfwInit()) {
+
         return -1;
     }
     window = glfwCreateWindow(640,480, "ESGI-OpenGL_Project_dev", nullptr, nullptr);
