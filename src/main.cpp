ddefd87b67adb4f342b5eaf9f49e24bce49d2802
Esilff <vbanari02@gmail.com>
2023-03-09 21:14:51 +0100

    adding basic GLFW window

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main() {
+    GLFWwindow * window;
+    if (!glfwInit()) {
+        return -1;
+    }
+    window = glfwCreateWindow(640,480, "ESGI-OpenGL_Project_dev", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    return 0;
+}
