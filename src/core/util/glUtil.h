a233c64f9f61bd56e0cf85659206f27f08269d99
Esilff <vbanari02@gmail.com>
2023-04-06 16:53:42 +0200

    adding glUtil headerfile

---
+++ src/core/util/glUtil.h
@@ -0,0 +1,15 @@
+//
+// Created by Esilff on 06/04/2023.
+//
+
+#ifndef GLUTIL_H
+#define GLUTIL_H
+#include <glad/glad.h>
+
+namespace GLUtil {
+    void GLAPIENTRY debugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, const GLchar* message, const void* userParam) {
+        
+    }
+}
+
+#endif //GLUTIL_H
