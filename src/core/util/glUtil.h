da7d4eaa4e3ea2c4849940533fbc9ca7100bb487
Esilff <vbanari02@gmail.com>
2023-04-06 17:22:03 +0200

    printing error on callback

--- src/core/util/glUtil.h
+++ src/core/util/glUtil.h
@@ -5,10 +5,11 @@
 #ifndef GLUTIL_H
 #define GLUTIL_H
 #include <glad/glad.h>
+#include <iostream>
 
 namespace GLUtil {
-    void GLAPIENTRY debugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, const GLchar* message, const void* userParam) {
-        
+    void debugCallback(unsigned int source, unsigned int type, unsigned int id, unsigned int severity, GLsizei length, const GLchar* message, const void* userParam) {
+        std::cout << "[OpenGL Error] : " << message << std::endl;
     }
 }
 
